a9c293b3899f8c8fb45938287e57039b3682b93d
Ilia Loginov <saft48@yandex.ru>
2023-08-06 14:30:12 +0300

    improving_feats_5.1 -> added command /stop

--- dialogs/__init__.py
+++ dialogs/__init__.py
@@ -1,4 +1,4 @@
-from . import main_menu
+from . import main_menu_cmds
 from . import memorize_event
 from . import remember_event
 from . import test_dialog   # ... pure testing part for new features;)
