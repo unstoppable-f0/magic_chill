c54cf1deb8db6dd2e000cfe913c4eb26a6adac32
Ilia Loginov <saft48@yandex.ru>
2023-11-12 13:23:05 +0300

    fixing circular import

--- dialogs/__init__.py
+++ dialogs/__init__.py
@@ -2,4 +2,8 @@ from . import main_menu_cmds
 from . import memorize_event
 from . import remember_event
 from . import quotes_of
-from . import test_dialog   # ... pure testing part for new features;)
+
+try:
+    from . import test_dialog   # ... pure testing part for new features;)
+except ImportError:
+    pass
