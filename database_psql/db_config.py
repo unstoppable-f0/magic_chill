fb8cdaa48c9c29d8c33789d57ca757b46df901a5
unstoppable-f0 <saft48@icloud.com>
2024-02-28 12:06:33 +0300

    Further understanding the ancient art of SQLAlchemy

--- database_psql/db_config.py
+++ database_psql/db_config.py
@@ -1,16 +1,18 @@
 import asyncio
 from sqlalchemy.ext import asyncio as async_alchemy
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from sqlalchemy import text
 
-
-test_url = "postgresql+asyncpg://unstoppable:BubbaMngloa#843@/magic_test"
-engine = async_alchemy.create_async_engine(
-    url=test_url,
-    echo=True,
-    )
+# With asynchronous code it's a little bit different --> you need to create an engine and then dispose of it
+# in a coroutine
 
 
-async def commit_as_go():
+
+
+async def create_commit_as_go(engine: AsyncEngine):
     """ 'Commit as you go'-style: uses Engine.commit(), so we need to write conn.commit() ourselves"""
     async with engine.connect() as conn:
         """ 'Commit as you go' --> thorugh """
@@ -20,7 +22,7 @@ async def commit_as_go():
         await conn.commit()
 
 
-async def begin_once():
+async def begin_once(engine: AsyncEngine):
     """ 'Begin once'-style  -> automatically commits or rollbacks"""
 
     async with engine.begin() as conn:
@@ -29,18 +31,70 @@ async def begin_once():
                            )
 
 
-async def print_table():
+async def print_table_all(engine: AsyncEngine):
     async with engine.connect() as conn:
         result = await conn.execute(text("""SELECT * FROM test_table"""))
         for i_row in result:
             print(i_row)
 
 
-async def main_db():
-    # await commit_as_go()
-    await begin_once()
-    await print_table()
+async def print_table_x_y(engine: AsyncEngine):
+    async with engine.connect() as conn:
+        result = await conn.execute(text("""SELECT x, y FROM test_table WHERE y > :y"""), [{'y': 2}])
+        for i_row in result:
+            print(i_row.x, i_row.y)
 
+        await engine.dispose()
+
+
+# async def print_table_orm():
+#     async with Session(engine) as session:
+
+
+async def main_db_core():
+    """Main function to operate SQL in"""
+
+    test_url = "postgresql+asyncpg://unstoppable:BubbaMngloa#843@/magic_test"
+    engine = async_alchemy.create_async_engine(
+        url=test_url,
+        echo=True,
+    )
+
+    # await create_commit_as_go(engine)
+    # await begin_once(engine)
+    # await print_table_all(engine)
+    await print_table_x_y(engine)
+
+    await engine.dispose()  # have to dispose of an async engine
+
+############################################################################################################
+
+
+async def insert_values_and_print(async_session: async_sessionmaker[AsyncSession]) -> None:
+    async with async_session.begin() as session:
+        await session.execute(text("""INSERT INTO test_table (x, y) VALUES (:x, :y)"""),
+                              [{'x': 21, 'y': 28}, {'x': 32, 'y': 44}]
+                              )
+
+        result = await session.execute(text("""SELECT * FROM test_table WHERE x > :x"""), [{'x': 20}])
+        for row in result:
+            print(row.x, row.y)
+
+
+async def main_db_orm():
+    """Main function to operate SQL in ORM-paradigm"""
+
+    test_url = "postgresql+asyncpg://unstoppable:BubbaMngloa#843@/magic_test"
+    engine = async_alchemy.create_async_engine(
+        url=test_url,
+        echo=True,
+    )
+
+    async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+    await insert_values_and_print(async_session)
+    await engine.dispose()
 
 if __name__ == '__main__':
-    asyncio.run(main_db())
+    # asyncio.run(main_db_core())
+    asyncio.run(main_db_orm())
